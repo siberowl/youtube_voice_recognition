dff009ecd1e9a0996450fc6ddda9dea5dbf16580
Kon Aoki <kon.aoki.us@gmail.com>
2021-08-21 08:03:12 -0700

    added print

--- speechrecognition.py
+++ speechrecognition.py
@@ -9,8 +9,8 @@ for i in range(4):
         audio_data = r.record(source)
         try:
             text = r.recognize_google(audio_data, language="ja-JP")
+            print("filename: "+filename+" text: "+text)
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
             print("Could not request results from Google Speech Recognition service; {0}".format(e))
-        print(text)
