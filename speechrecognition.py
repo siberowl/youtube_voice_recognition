132f9697b69608662bf3e08a5a1f4eb82ebf18af
Kon Aoki <kon.aoki.us@gmail.com>
2021-08-21 07:54:50 -0700

    looping through audio and added error handling

--- speechrecognition.py
+++ speechrecognition.py
@@ -1,9 +1,16 @@
 import speech_recognition as sr
-filename = "./sample_audio/sample1.wav"
 # initialize the recognizer
 r = sr.Recognizer()
-# open the file
-with sr.AudioFile(filename) as source:
-    audio_data = r.record(source)
-    text = r.recognize_google(audio_data, language="ja-JP")
-    print(text)
+
+for i in range(4):
+    filename = "./sample_audio/sample"+str(i+1)+".wav"
+    # open the file
+    with sr.AudioFile(filename) as source:
+        audio_data = r.record(source)
+        try:
+            text = r.recognize_google(audio_data, language="ja-JP")
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        print(text)
