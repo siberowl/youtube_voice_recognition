2450c934722887b87a7818d8a3b60f28220e3aaf
Kon Aoki <kon.aoki.us@gmail.com>
2021-08-21 08:08:07 -0700

    formatted

--- speechrecognition.py
+++ speechrecognition.py
@@ -1,16 +1,21 @@
 import speech_recognition as sr
+
 # initialize the recognizer
 r = sr.Recognizer()
 
 for i in range(4):
-    filename = "./sample_audio/sample"+str(i+1)+".wav"
+    filename = "./sample_audio/sample" + str(i + 1) + ".wav"
     # open the file
     with sr.AudioFile(filename) as source:
         audio_data = r.record(source)
         try:
             text = r.recognize_google(audio_data, language="ja-JP")
-            print("filename: "+filename+" text: "+text)
+            print("filename: " + filename + " text: " + text)
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+            print(
+                "Could not request results from Google Speech Recognition service; {0}".format(
+                    e
+                )
+            )
