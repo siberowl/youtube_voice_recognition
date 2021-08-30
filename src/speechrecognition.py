698151ce59283a3d56f15bcb915aa2cf4592bbce
Kon Aoki <kon.aoki.us@gmail.com>
2021-08-30 08:06:07 -0700

    removed saving function and changed to snake case

--- src/speechrecognition.py
+++ src/speechrecognition.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 
-def getText(audio_path):
+
+def get_text(audio_path):
     # initialize the recognizer
     r = sr.Recognizer()
     # open the file
@@ -13,9 +14,3 @@ def getText(audio_path):
             return "Google Speech Recognition could not understand audio"
         except sr.RequestError:
             return "Could not request results from Google"
-
-
-def save_speech(output_path, audio_path):
-    f = open(output_path, "w")
-    text = getText(audio_path)
-    f.write(text)
