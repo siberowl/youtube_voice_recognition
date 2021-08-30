27e5f4fe21bf03e52cec1ace55217eab16932fae
Kon Aoki <kon.aoki.us@gmail.com>
2021-08-30 08:10:47 -0700

    recognizing speech and outputting for each segment

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from youtubedownloader import download_audio
 from chatdownloader import download_chat
-from speechrecognition import save_speech
+from speechrecognition import get_text
 from segmenter import save_segment
 from urllib.parse import urlparse
 from urllib.parse import parse_qs
@@ -15,7 +15,6 @@ query = parse_qs(url_data.query)
 basepath = path.dirname(__file__)
 chat_output_path = path.abspath(path.join(basepath, "..", "chat", query["v"][0] + ".chat"))
 audio_output_path = path.abspath(path.join(basepath, "..", "audio", query["v"][0] + ".wav"))
-audio_segment_output_path = path.abspath(path.join(basepath, "..", "audio", query["v"][0] + ".segment.wav"))
 speech_output_path = path.abspath(path.join(basepath, "..", "speech", query["v"][0] + ".speech"))
 
 print("Downloading audio...")
@@ -25,5 +24,14 @@ download_chat(chat_output_path, url)
 
 chat_file = open(chat_output_path, "r")
 chat_lines = chat_file.readLines()
+speech_file = open(speech_output_path, "w")
 for line in chat_lines:
-    pass
+    timestamp = float(line.split(",")[0]) * 1000
+    message = line.split(",")[1]
+    audio_segment_output_path = path.abspath(
+        path.join(basepath, "..", "audio", query["v"][0] + "." + timestamp + ".wav")
+    )
+    save_segment(audio_output_path, audio_segment_output_path, timestamp, timestamp + delay * 1000)
+    speech = get_text(audio_segment_output_path)
+    speech_file.write(speech + "\n")
+speech.close()
