39abb0f5a302dabe0a6a056808a6d783b91674fd
Kon Aoki <kon.aoki.us@gmail.com>
2021-08-30 06:27:25 -0700

    downloading chat messages

--- src/chatdownloader.py
+++ src/chatdownloader.py
@@ -1,6 +1,10 @@
 from chat_downloader import ChatDownloader
+import re
 
 url = "https://youtu.be/RqDU86c7Nu4"
+f = open(re.sub("[^A-Za-z0-9]+", "", url) + "_chat.json", "w")
 chat = ChatDownloader().get_chat(url)  # create a generator
 for message in chat:  # iterate over messages
     chat.print_formatted(message)  # print the formatted message
+    f.write(str(message["time_in_seconds"]) + ", " + message["message"] + "\n")
+f.close()
